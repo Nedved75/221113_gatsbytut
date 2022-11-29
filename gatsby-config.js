1d7ae5e66676faacee85a5ee86754704fb57b865
akott <an.kotter@gmail.com>
2022-11-29 19:14:12 +0100

    jh

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,11 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
+    title: `Some Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
